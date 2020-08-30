87b5f636167ad067303e33c575186a823d4c4a6f
Tyler Hines <tylerhines@zoneandco.com>
2020-08-30 10:43:21 -0400

    added Ability class to new file

---
+++ lol_cd_quiz/Ability.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+namespace lol_cd_quiz
+{
+    public class Ability
+    {
+        public Ability(HtmlNode node)
+        {
+            Name = GetName(node);
+            Key = GetKey(node);
+            Cooldown = GetCooldown(node);
+            Description = GetDescription(node);
+        }
+
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public string Cooldown { get; set; }
+        public string Description { get; set; }
+
+        public String GetCooldown(HtmlNode node)
+        {
+            var cooldown = "";
+
+            foreach (HtmlNode element in node.ChildNodes)
+            {
+                if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__cooldown")
+                {
+                    var cooldowns = new string((from i in element.InnerText
+                                                where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
+                                                select i
+                                                ).ToArray()).Replace("\n", "");
+                    var cooldownArray = cooldowns.Split(" ");
+                    cooldown = cooldownArray[0];
+
+                    if (cooldown == "")
+                    {
+                        cooldown = "0";
+                    }
+                }
+
+            }
+
+            return cooldown;
+        }
+        public string GetName(HtmlNode node)
+        {
+            var name = "";
+
+            foreach (HtmlNode element in node.ChildNodes)
+            {
+                if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__name")
+                {
+                    var nameString = new string((from i in element.ChildNodes[0].InnerText
+                                                 where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
+                                                 select i
+                                                ).ToArray()).Replace("\n", " ");
+                    var nameArray = nameString.Split(" ");
+                    name = nameString.Trim();
+                }
+
+            }
+
+            return name;
+        }
+
+        public string GetKey(HtmlNode node)
+        {
+            var key = "Passive";
+
+            foreach (HtmlNode element in node.ChildNodes)
+            {
+                if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__letter")
+                {
+                    var keyString = new string((from i in element.ChildNodes[0].InnerText
+                                                where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
+                                                select i
+                                                ).ToArray()).Replace("\n", " ");
+                    key = keyString.Trim();
+                }
+
+            }
+
+            return key;
+        }
+
+        public string GetDescription(HtmlNode node)
+        {
+            var desc = "";
+
+            foreach (HtmlNode element in node.ChildNodes)
+            {
+                if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__desc")
+                {
+                    var descString = element.ChildNodes.ToList();
+                    var newDesc = descString.FindAll(x => x.Name == "#text");
+
+                    foreach (var descElement in newDesc)
+                    {
+                        desc += descElement.InnerText;
+                    }
+                }
+
+            }
+
+            return desc;
+        }
+    }
+}
--- lol_cd_quiz/Program.cs
+++ lol_cd_quiz/Program.cs
@@ -7,10 +7,8 @@ using HtmlAgilityPack;
 using MongoDB.Driver;
 using MongoDB.Driver.Core;
 using MongoDB.Driver.Linq;
-using System.Data.SqlClient;
 using LanguageExt;
 using MongoDB.Bson;
-using System.Web;
 
 namespace lol_cd_quiz
 {
@@ -123,39 +121,14 @@ namespace lol_cd_quiz
             champion.Name = GetHtmlNode(url, "//div[contains(@class, 'champ-splash__title')]/h2").InnerText;
             champion.Abilities = new List<Ability>();
             var championAbilityNodes = GetHtmlNodes(url, "//a[contains(@class, 'champ-abilities__item ')]");
-            var championCooldownNodes = GetHtmlNodes(url, "//div[contains(@class, 'champ-abilities__item__cooldown')]");
 
             foreach (HtmlNode node in championAbilityNodes)
             {
-                var ability = GetAbilityInfo(node);
+                var ability = new Ability(node);
                 champion.Abilities.Add(ability);
             }
 
             return champion;
-        }
-
-        private static Ability GetAbilityInfo(HtmlNode node)
-        {
-            Ability ability = new Ability();
-            ability.Cooldown = ability.GetCooldown(node);
-            ability.Name = ability.GetName(node);
-            ability.Key = ability.GetKey(node);
-            ability.Description = ability.GetDescription(node);
-
-            return ability;
-        }
-
-        private static String GetChampionCooldown(HtmlNode node)
-        {
-            var nodeText = node.SelectSingleNode("//div[contains(@class, 'champ-abilities__item__cooldown')]").InnerText.Replace("\n", "");
-            var cooldowns = new string((from i in nodeText
-                                       where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
-                                       select i
-                                       ).ToArray()).Replace(Environment.NewLine, "");
-            var cooldownArray = cooldowns.Split(" ");
-            var cooldown = cooldownArray[0];
-
-            return cooldown;
         }
 
         private static IMongoDatabase ConnectMongoDb()
@@ -172,102 +145,6 @@ namespace lol_cd_quiz
             public ObjectId _id { get; set; }
             public string Name { get; set; }
             public List<Ability> Abilities { get; set; }
-        }
-
-        public class Ability
-        {
-            public string Name { get; set; }
-            public string Key { get; set; }
-            public string Cooldown { get; set; }
-            public string Description { get; set; }
-
-            public String GetCooldown(HtmlNode node)
-            {
-                var cooldown = "";
-
-                foreach (HtmlNode element in node.ChildNodes)
-                {
-                    if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__cooldown")
-                    {
-                        var cooldowns = new string((from i in element.InnerText
-                                                    where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
-                                                    select i
-                                                    ).ToArray()).Replace("\n", "");
-                        var cooldownArray = cooldowns.Split(" ");
-                        cooldown = cooldownArray[0];
-
-                        if (cooldown == "")
-                        {
-                            cooldown = "0";
-                        }
-                    }
-
-                }
-
-                return cooldown;
-            }
-            public string GetName(HtmlNode node)
-            {
-                var name = "";
-
-                foreach (HtmlNode element in node.ChildNodes)
-                {
-                    if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__name")
-                    {
-                        var nameString = new string((from i in element.ChildNodes[0].InnerText
-                                                    where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
-                                                    select i
-                                                    ).ToArray()).Replace("\n", " ");
-                        var nameArray = nameString.Split(" ");
-                        name = nameString.Trim();
-                    }
-
-                }
-
-                return name;
-            }
-
-            public string GetKey(HtmlNode node)
-            {
-                var key = "Passive";
-
-                foreach (HtmlNode element in node.ChildNodes)
-                {
-                    if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__letter")
-                    {
-                        var keyString = new string((from i in element.ChildNodes[0].InnerText
-                                                     where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
-                                                     select i
-                                                    ).ToArray()).Replace("\n", " ");
-                        key = keyString.Trim();
-                    }
-
-                }
-
-                return key;
-            }
-
-            public string GetDescription(HtmlNode node)
-            {
-                var desc = "";
-
-                foreach (HtmlNode element in node.ChildNodes)
-                {
-                    if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__desc")
-                    {
-                        var descString = element.ChildNodes.ToList();
-                        var newDesc = descString.FindAll(x => x.Name == "#text");
-
-                        foreach (var descElement in newDesc)
-                        {
-                            desc += descElement.InnerText;
-                        }
-                    }
-
-                }
-
-                return desc;
-            }
         }
     }
 }
