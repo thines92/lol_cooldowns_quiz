e471a83329df43024c3ab79275a708dae4d48bca
Tyler Hines <tylerhines@zoneandco.com>
2020-08-30 20:37:20 -0400

    refactor GetCost

--- lol_cd_quiz/Ability.cs
+++ lol_cd_quiz/Ability.cs
@@ -21,9 +21,9 @@ namespace lol_cd_quiz
         public string IconSrc { get; set; }
         public string Name { get; set; }
         public string Key { get; set; }
-        public List<String> Cooldowns { get; set; }
+        public List<string> Cooldowns { get; set; }
         public string Description { get; set; }
-        public string Cost { get; set; }
+        public List<string> Cost { get; set; }
         public List<string> Range { get; set; }
 
         public string GetIconSrc(HtmlNode node)
@@ -96,27 +96,17 @@ namespace lol_cd_quiz
             return description;
         }
 
-        public string GetCost(HtmlNode node)
+        public List<string> GetCost(HtmlNode node)
         {
-            var cost = "";
+            Filter filter = new Filter("class", "champ-abilities__item__cost");
 
-            foreach (HtmlNode element in node.ChildNodes)
-            {
-                if (element.Attributes["class"] != null && element.Attributes["class"].Value == "champ-abilities__item__cost")
-                {
-                    var costString = new string((from i in element.ChildNodes[0].InnerText
-                                                where char.IsLetterOrDigit(i) || char.IsWhiteSpace(i)
-                                                select i
-                                                ).ToArray()).Replace("\n", " ");
-                    var costArray = costString.Split(" ").Where(x => x.ToString() != "").ToArray();
-                    if (costArray.Length > 0)
-                    {
-                        cost = costArray[0];
-                    }
-                }
-
-            }
-
+            var cost = HtmlScraper.SearchNodeForValue(node, filter)
+                .Where(x => x.Name.Contains("text"))
+                .FirstOrDefault()
+                .InnerText.Replace("\n", "").Replace(" / ", " ")
+                .Split(" ")
+                .ToList();
+      
             return cost;
         }
 
